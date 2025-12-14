eb519d50e0b41b9a03a89b4f78f21342beee1c19
Starinin Andrey <starinin-andrey@ya.ru>
2025-12-14 10:25:14 +0300

    Создание контекста для БД

---
+++ AccountManagerApp.DAL/DataBaseContext.cs
@@ -0,0 +1,12 @@
+using AccountManagerApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountManagerApp.DAL;
+
+public class DataBaseContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+}
---
+++ AccountManagerApp.DAL/DataBaseContextFactory.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace AccountManagerApp.DAL;
+
+public class DataBaseContextFactory : IDesignTimeDbContextFactory<DataBaseContext>
+{
+    public DataBaseContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = config.GetConnectionString("DefaultConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<DataBaseContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new DataBaseContext(optionsBuilder.Options);
+    }
+}
