a3cabae70de7e371c7f72bf59b6e4f1b3fd80c93
Starinin Andrey <starinin-andrey@ya.ru>
2025-12-14 10:14:40 +0300

    Добавление проекта с моделями данных

---
+++ AccountManagerApp.Models/Account.cs
@@ -0,0 +1,12 @@
+namespace AccountManagerApp.Models;
+
+public class Account
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string? Description { get; set; }
+    public bool IsActive { get; set; } = true;
+
+    public User User { get; set; }
+}
---
+++ AccountManagerApp.Models/User.cs
@@ -0,0 +1,13 @@
+namespace AccountManagerApp.Models;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string Email { get; set; }
+    public ICollection<string> PhoneNumbers { get; set; } = [];
+    public bool IsActive { get; set; } = true;
+
+    public Account Account { get; set; }
+}
